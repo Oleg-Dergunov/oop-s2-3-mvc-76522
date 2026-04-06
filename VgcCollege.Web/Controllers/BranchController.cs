a12cfe5d9ff46d4aa31ba97b1d99272d709fb8b5
Oleg Dergunov <oleg.dergunov@outlook.com>
2026-04-06 13:46:31 +0100

    Add Branch CRUD, improve Attendance with status enum and rate calculation

---
+++ VgcCollege.Web/Controllers/BranchController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VgcCollege.Web.Controllers
+{
+    public class BranchController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
