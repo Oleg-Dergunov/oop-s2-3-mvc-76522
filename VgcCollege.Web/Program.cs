d59e98c1a91da18093f072b16d69600b087b61c3
Oleg Dergunov <oleg.dergunov@outlook.com>
2026-04-06 23:34:03 +0100

    Add global exception handling middleware, improve error page

--- VgcCollege.Web/Program.cs
+++ VgcCollege.Web/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using VgcCollege.Web.Data;
+using VgcCollege.Web.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -41,6 +42,7 @@ else
     app.UseHsts();
 }
 
+app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
